9776e43696825ac73b7ac1042119dcea2977c40f
tmcuong2408 <tmcuong2408@gmail.com>
2026-09-26 02:35:02 +0700

    Auto backup: 26/09/2026  2:35:02.23

---
+++ autopush.bat
@@ -0,0 +1,13 @@
+:: Thêm tất cả thay đổi
+git add .
+
+:: Commit với ngày tháng
+git commit -m "Auto backup: %DATE% %TIME%"
+
+:: Push lên branch hiện tại
+git push
+
+echo ========================================
+echo   Hoan thanh!
+echo ========================================
+pause
